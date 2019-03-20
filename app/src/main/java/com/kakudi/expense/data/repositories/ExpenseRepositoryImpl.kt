36876815fb00f4c4d265a376033341220341577d
Meshileya Seun <meshileyaseun@gmail.com>
2019-03-20 16:02:27 +0100

    update expense injection

--- app/src/main/java/com/kakudi/expense/data/repositories/ExpenseRepositoryImpl.kt
+++ app/src/main/java/com/kakudi/expense/data/repositories/ExpenseRepositoryImpl.kt
@@ -3,13 +3,14 @@ package com.kakudi.expense.data.repositories
 import com.kakudi.expense.data.model.Expense
 import com.kakudi.expense.data.sources.db.ExpenseLocalResource
 import io.reactivex.Observable
+import javax.inject.Inject
 import javax.inject.Named
 
 /**
  *@author meshileya seun <mesh@kudi.ai/>
  *@date 17/03/2019
  */
-class ExpenseRepositoryImpl(@Named("localExpenseResource") private val localResource: ExpenseLocalResource) :
+class ExpenseRepositoryImpl @Inject constructor(@Named("localExpenseResource") private val localResource: ExpenseLocalResource) :
     ExpenseRepository {
 
     override fun getAllExpenses(userId: String): Observable<List<Expense>> {
